8d667235fc978c38c989c317fe25a64b479704ba
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-06 16:20:50 +0530

    Add files via upload
    All the facts stored in a list in this file.
    Runs without any bugs!

---
+++ trivia.py
@@ -0,0 +1,36 @@
+
+import random
+facts = [
+    "Dead skin cells are a main ingredient in household dust",
+    "Sudan has more pyramids than any country in the world",
+    "The bumblebee bat is the worlds smallest mammal",
+    "The circulatory system is more than 60,000 miles long",
+    "The worlds first animated feature film was made in Argentina - It was a political satire called El Apóstol made up of 58,000 drawings and had a running time of 70 minutes",
+    "The Philippines consists of 7,641 islands",
+    "There is enough gold inside Earth to coat the planet",
+    "It takes a drop of water 90 days to travel the entire Mississippi River - Spanning 2,340 miles, the Mississippi River is the third-largest watershed in the world",
+    "Lemons float, but limes sink - limes are denser than lemons",
+    "Some fungi create zombies, then control their minds",
+    "The longest English word is 189,819 letters long -  the full name for the protein nicknamed titin would take three and a half hours to say out loud",
+    "A one-way trip on the Trans-Siberian Railway involves crossing 3,901 bridges",
+    "No number before 1,000 contains the letter A",
+    "The # symbol isnt officially called hashtag - Its technical name is octothorpe",
+    "The inventor of the microwave appliance received only $2 for his discovery - Percy Spencer was working as a researcher for American Appliance Company (now Raytheon) ",
+    "Bees can fly higher than Mount Everest",
+    "The first commercial passenger flight lasted only 23 minutes",
+    "Australia is wider than the moon",
+    "Venus is the only planet to spin clockwise",
+    "The Spanish national anthem has no words",
+    "You can hear a blue whale's heartbeat from over 2 miles away",
+    "Ketchup was once sold as medicine",
+    "The shortest commercial flight in the world is in Scotland",
+    "The strongest muscle in the body is the tongue",
+    "Coca-Cola was originally green",
+    "Wearing headphones for just an hour will increase the bacteria in your ear by 700 times",
+    "Butterflies taste with their feet",
+    "A snail can sleep for three years",
+    "Each king in a deck of playing cards represents a great king from history."
+
+]
+
+
