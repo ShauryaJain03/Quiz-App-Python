1d1db781b89bfa4efb76135da56f621062954340
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-07 02:59:32 +0530

    Update MainPage.py
    Genre page basic structure has been added.
    Pyimage is still there.

--- MainPage.py
+++ MainPage.py
@@ -7,16 +7,17 @@ from trivia import *
 global bg
 global photo1
 global photo2
+
 # Create the main window and run the app
 root = CTk()
 root.title("Quizzify")
 root.geometry("680x620")  #width vs height
 root.resizable(False,False)
-root.iconbitmap("C:\\Users\\shaur\\OneDrive\\Desktop\\Project\\Images\\iconfilemain\\favicon.ico")
+root.iconbitmap("C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\iconfilemain\\favicon.ico")
 set_appearance_mode("system")
 set_default_color_theme("dark-blue") 
 
-bg=PhotoImage(file="C:\\Users\\shaur\\OneDrive\\Desktop\\Project\\Images\\background2.png")
+bg=PhotoImage(file="C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\background2.png")
 
 # Get the entered username and password using get function
 def login():
@@ -26,29 +27,39 @@ def login():
     root.withdraw()
     mainmenu()
 
-
-
+def genre():
+    genrepage=CTkToplevel(root)
+    genrepage.title("Choose your genre")
+    genrepage.geometry("680x620")
+    genrepage.resizable(False,False)    
+    genrepage.iconbitmap("C:\\Users\\shaur\\OneDrive\Desktop\\DS Project\\Images\\iconfilemain\\favicon.ico")
+    image3=Image.open("C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\background.png")
+    photo3=ImageTk.PhotoImage(image3)
+    label3=Label(genrepage,image=photo3)
+    label3.image=photo3
+    label3.pack()      
 
 def mainmenu():
     menu=CTkToplevel(root)
     menu.title("Main Menu")
     menu.geometry("680x620")
     menu.resizable(False,False)    
-    menu.iconbitmap('C:\\Users\\shaur\\OneDrive\\Desktop\\Project\\Images\\iconfilemain\\favicon.ico')
-    image1=Image.open('C:\\Users\\shaur\\OneDrive\\Desktop\\Project\\Images\\background.png')
+    menu.iconbitmap("C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\iconfilemain\\favicon.ico")
+    image1=Image.open("C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\background.png")
     photo1=ImageTk.PhotoImage(image1)
     label1=Label(menu,image=photo1)
     label1.image=photo1
     label1.pack()
-    playbtn=CTkButton(menu,text="Play",fg_color="#97e2e8",border_width=1,text_color="#000",border_color="#feaf88",height=40,width=115, font=("helvetica",18)).place(relx=0.4,rely=0.3)
+    playbtn=CTkButton(menu,text="Play",fg_color="#97e2e8",border_width=1,command=genre,text_color="#000",border_color="#feaf88",height=40,width=115, font=("helvetica",18)).place(relx=0.4,rely=0.3)
     leadbtn=CTkButton(menu,text="Leaderboard",fg_color="#97e2e8",border_width=1,text_color="#000",border_color="#feaf88",height=40,width=115, font=("helvetica",18)).place(relx=0.4,rely=0.4)
     trivbtn=CTkButton(menu,text="Trivia",command=trivia,fg_color="#97e2e8",border_width=1,border_color="#feaf88",text_color="#000",height=40,width=115, font=("helvetica",18)).place(relx=0.4,rely=0.5)
     exitbtn=CTkButton(menu,text="Exit",command=lambda:[menu.destroy(),root.quit()],fg_color="#97e2e8",border_width=1,text_color="#000",border_color="#feaf88",height=40,width=115, font=("helvetica",18)).place(relx=0.4,rely=0.6)
 
+
+
 def display_random_fact():
     random_fact = random.choice(facts)
     fact_var.set(random_fact)    
-
 
 def trivia():
     global trivia_page
@@ -56,8 +67,8 @@ def trivia():
     trivia_page.title("DID YOU KNOW?")
     trivia_page.geometry("680x471") 
     trivia_page.resizable(False,False)
-    trivia_page.iconbitmap('C:\\Users\\shaur\\OneDrive\\Desktop\\Project\\Images\\iconfilemain\\favicon.ico')
-    image2=Image.open("C:\\Users\\shaur\\OneDrive\\Desktop\\Project\\Images\\mainpage.jpg")
+    trivia_page.iconbitmap("C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\iconfilemain\\favicon.ico")
+    image2=Image.open("C:\\Users\\shaur\\OneDrive\\Desktop\\DS Project\\Images\\mainpage.jpg")
     photo2=ImageTk.PhotoImage(image2)
     label2=Label(trivia_page,image=photo2)
     label2.image=photo2
@@ -65,10 +76,10 @@ def trivia():
     global fact_label
     global fact_var
     fact_var = StringVar()
-    fact_label=CTkLabel(trivia_page,textvariable=fact_var,text_color="black",fg_color="aqua").place(relx=0.5,rely=0.5)
+    fact_label=CTkLabel(trivia_page,textvariable=fact_var,text_color="red",fg_color="black",wraplength=250,font=("helvetica",23)).place(relx=0.2,rely=0.2)
     triv_close=CTkButton(trivia_page,text="Close",command=trivia_page.destroy,height=30,width=100,text_color="#fff", font=("helvetica",18))
-    triv_close.place(relx=0.4,rely=0.9)
-    generate_button=CTkButton(trivia_page,text="generate",command=display_random_fact).place(relx=0.7,rely=0.9)
+    triv_close.place(relx=0.5,rely=0.9)
+    generate_button=CTkButton(trivia_page,text="Generate",height=30,width=100,command=display_random_fact,font=("helvetica",18)).place(relx=0.25,rely=0.9)
 
 #create label
 mylabel=Label(root,image=bg)
