d47b0c14c646cb7fdcda96670c323db96ed87f48
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-07 18:02:12 +0530

    Add files via upload
    GK page , which will have the main game logic.

---
+++ GK.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from customtkinter import *
+def gk():
+    gkpage=CTkToplevel()
+    gkpage.title("General Knowledge")
+    gkpage.geometry("680x620")
+    gkpage.attributes("-topmost", True)
