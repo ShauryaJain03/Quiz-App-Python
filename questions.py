6e9552d3476e6f91e91b24b86b40b462c1969e53
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-07 23:57:07 +0530

    Add files via upload
    Working game logic!!

---
+++ questions.py
@@ -0,0 +1,7 @@
+
+all_questions=[
+    ["What is the unit of electric charge?", 0,["Coulumb", "Ampere", "electron volt", "Watt"]],
+    ["What is the SI unit of frequency?",2, ["Joule", "Watt", "Hertz", "Metre"]],
+    ["What is the process of converting a solid directly into a gas called?",1,  ["Condensation", "Sublimation", "Evaporation", "Vapourisation"]],
+    ["What is the functional unit of the nervous system?", 0, ["Neurons", "Dendrite", "Glial cells", "Soma"]],
+    ["What is the functional unit of the kidney?", 0, ["Nephrons", "Urinary Bladder", "Urethra", "Ureters"]]]
---
+++ trial.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import random
+import questions
+from tkinter import messagebox
+
+def display_questions():
+    selected_questions = random.sample(questions.all_questions, 3)
+    question_index = 0
+    score = 0
+
+    def check_answer():
+        nonlocal score
+
+        selected_option = options.get()
+        correct_option = selected_questions[question_index][1]
+
+        if selected_option == correct_option:
+            score += 1
+
+        next_question()
+
+    def next_question():
+        nonlocal question_index
+
+        if question_index < len(selected_questions) - 1:
+            question_index += 1
+            question_label.config(text=selected_questions[question_index][0])
+            options.set(-1)
+
+            for i, option in enumerate(option_buttons):
+                option.configure(text=selected_questions[question_index][2][i])
+
+            score_label.config(text="Score: {}".format(score))
+        else:
+            messagebox.showinfo("Quiz", "Quiz completed!\nYour score: {}".format(score))
+            window.destroy()
+
+    window = tk.Tk()
+    window.title("Quiz")
+
+    question_label = tk.Label(window, text=selected_questions[question_index][0])
+    question_label.pack(pady=10)
+
+    options = tk.IntVar()
+    options.set(-1)
+
+    option_buttons = []
+    for i in range(4):
+        option_button = tk.Radiobutton(window, text="", variable=options, value=i)
+        option_button.pack(anchor='w')
+        option_buttons.append(option_button)
+
+    check_button = tk.Button(window, text="Check Answer", command=check_answer)
+    check_button.pack(pady=10)
+
+    score_label = tk.Label(window, text="Score: 0")
+    score_label.pack()
+
+    next_question()
+
+    window.mainloop()
+
+display_questions()
