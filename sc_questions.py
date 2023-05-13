599c2bf041917b47317bf47ee9c3a6dd63026767
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-14 02:23:35 +0530

    Update and rename questions.py to sc_questions.py
    Added questions

---
+++ sc_questions.py
@@ -0,0 +1,13 @@
+all_questions = [
+    ("What is the unit of electric charge?", 0, ["Coulumb", "Ampere", "electron volt", "Watt"]),
+    ("What is the SI unit of frequency?", 2, ["Joule", "Watt", "Hertz", "Metre"]),
+    ("What is the process of converting a solid directly into a gas called?", 1, ["Condensation", "Sublimation", "Evaporation", "Vapourisation"]),
+    ("What is the functional unit of the nervous system?", 0, ["Neurons", "Dendrite", "Glial cells", "Soma"]),
+    ("What is the functional unit of the kidney?", 0, ["Nephrons", "Urinary Bladder", "Urethra", "Ureters"]),
+    ("Which of the following is an example of an electromagnetic wave?",0, ["X-ray", "Sound wave", "Water wave", "Seismic wave"]),
+    ("Which of the following elements has the highest electronegativity?",1, ["Sodium (Na)", "Chlorine (Cl)", "Carbon (C)", "Oxygen (O)"]),
+    ("Which of the following is a characteristic of an acid?",0, ["It donates protons (H+ ions).", " It donates electrons.", "It accepts protons (H+ ions).", "It accepts electrons."]),
+    ("Which of the following is the primary function of the circulatory system?",0, ["Oxygen transport", "Digestion", "Hormone production", "Waste elimination"]),
+    ("Which of the following is responsible for carrying oxygen in red blood cells?",2, ["Platelets", " White blood cells", "Hemoglobin", "Plasma"])
+]
+
