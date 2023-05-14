87ff2f591c14946c190137beabf34a9f438acdd8
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-15 02:11:10 +0530

    Update sc_questions.py
    Added more questions

--- sc_questions.py
+++ sc_questions.py
@@ -8,6 +8,13 @@ all_questions = [
     ("Which of the following elements has the highest electronegativity?",1, ["Sodium (Na)", "Chlorine (Cl)", "Carbon (C)", "Oxygen (O)"]),
     ("Which of the following is a characteristic of an acid?",0, ["It donates protons (H+ ions).", " It donates electrons.", "It accepts protons (H+ ions).", "It accepts electrons."]),
     ("Which of the following is the primary function of the circulatory system?",0, ["Oxygen transport", "Digestion", "Hormone production", "Waste elimination"]),
-    ("Which of the following is responsible for carrying oxygen in red blood cells?",2, ["Platelets", " White blood cells", "Hemoglobin", "Plasma"])
+    ("Which of the following is responsible for carrying oxygen in red blood cells?",2, ["Platelets", " White blood cells", "Hemoglobin", "Plasma"]),
+    ("Which of the following is the primary greenhouse gas responsible for climate change?",0 , ["Carbon dioxide (CO2)", "Methane (CH4)", "Nitrous oxide (N2O)", "Ozone (O3)"]),
+    ("Which of the following organs produces insulin in the human body?",1 , ["Liver", "Pancreas", "Kidney", "Stomach"]),
+    ("Which of the following is an example of an exothermic reaction?", 0, ["Burning of wood", "Dissolution of sugar in water", "Photosynthesis", "Melting of ice"]),
+    ("What is the function of the cerebellum in the human brain?", 1, ["Control of voluntary movements", "Regulation of body temperature", "Production of hormones", "Processing of sensory information"]),
+    ("What does the term Schwarzschild radius represent?", 3, ["Radius of atom", "Radius of an electron", "Radius of a galaxy", "Radius of the event horizon"]),
+    ("What is the fundamental force responsible for radioactive decay?",2 , ["Strong Nuclear force", "Magnetic force", "Weak Nuclear force", "Electromagnetic"]),
+    ("What is the process by which an atomic nucleus releases energy by splitting into two smaller nuclei?", 3, ["Fusion", "Collision", "Annhilation", "Fission"])
 ]
 
