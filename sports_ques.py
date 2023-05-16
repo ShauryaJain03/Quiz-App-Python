0f1b78b4c5fe3988c2f31b8d19b7d846c745fc88
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-17 00:21:39 +0530

    Update sports_ques.py
    Structure of question tuple updated.

--- sports_ques.py
+++ sports_ques.py
@@ -1,10 +1,10 @@
-easy_ques=[
-    ("Who won the first FIFA World Cup ?",0,["Uruguay", "Brazil", "Argentina", "Chile"]),
-    ("Who is the most decorated Olympian of all time?",1,["Usain Bolt", "Michael Phelps", "Simone Biles", "Carl Lewis"]),
-    ("Who won the UEFA Champions League in 2021?",0,["Real Madrid", "Manchester City", "Chelsea", "PSG"]),
-    ("Who won the 2019 FIFA Women's World Cup?", 0, ["United States", "Germany", "England", "Brazil"]),
-    ("Who won the Super Bowl in 2020?", 0, ["Kansas City Chiefs", "San Francisco 49ers", "New England Patriots", "Philadelphia Eagles"]),
-    ("Who holds the record for the most Grand Slam titles in men's tennis?", 0, ["Roger Federer", "Novak Djokovic", "Rafael Nadal", "Pete Sampras"]),
-    ("Who is the all-time leading scorer in the history of English Premier League?", 0, ["Alan Shearer", "Wayne Rooney", "Thierry Henry", "Andy Cole"]),
-    ("Who holds the record for the fastest 100-meter dash in athletics?", 0, ["Usain Bolt", "Florence Griffith-Joyner", "Carl Lewis", "Justin Gatlin"]),
+all_questions=[
+    ("Who won the first FIFA World Cup ?",0,["Uruguay", "Brazil", "Argentina", "Chile"],"medium"),
+    ("Who is the most decorated Olympian of all time?",1,["Usain Bolt", "Michael Phelps", "Simone Biles", "Carl Lewis"],"easy"),
+    ("Who won the UEFA Champions League in 2021?",0,["Real Madrid", "Manchester City", "Chelsea", "PSG"],"medium"),
+    ("Who won the 2019 FIFA Women's World Cup?", 0, ["United States", "Germany", "England", "Brazil"],"medium"),
+    ("Who won the Super Bowl in 2020?", 0, ["Kansas City Chiefs", "San Francisco 49ers", "New England Patriots", "Philadelphia Eagles"],"hard"),
+    ("Who holds the record for the most Grand Slam titles in men's tennis?", 0, ["Roger Federer", "Novak Djokovic", "Rafael Nadal", "Pete Sampras"],"easy"),
+    ("Who is the all-time leading scorer in the history of English Premier League?", 0, ["Alan Shearer", "Wayne Rooney", "Thierry Henry", "Andy Cole"],"medium"),
+    ("Who holds the record for the fastest 100-meter dash in athletics?", 0, ["Usain Bolt", "Florence Griffith-Joyner", "Carl Lewis", "Justin Gatlin"],"easy"),
 ]
