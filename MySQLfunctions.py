d5fa8f2206b0bfc966de0185d9c6f09d34bb9e72
Shaurya Jain <112798681+ShauryaJain03@users.noreply.github.com>
2023-05-31 19:56:22 +0530

    Update MySQLfunctions.py
    Table creation on the basis of past existence with function parameter passing successful.

--- MySQLfunctions.py
+++ MySQLfunctions.py
@@ -15,18 +15,32 @@ def create_connection():
         print(f'Error connecting to MySQL database: {e}')
     return connection
 
-def insert_response(user_responseList):
+def insert_response(id,user_responseList):
     connection = create_connection()
     if connection:
         try:
             cursor = connection.cursor()
-            table_name = 'testdata'
-            for i in user_responseList:
-                data_list=i
-                placeholders = ', '.join(['%s'] * len(data_list))
-                query = f"INSERT INTO {table_name} VALUES ({placeholders})"
-                cursor.execute(query, data_list)
-                connection.commit()
+            cursor.execute("show tables like %s",(id,))
+            table_exists = cursor.fetchone()
+            if table_exists:
+                table_name = id
+                for i in user_responseList:
+                    data_list=i
+                    placeholders = ', '.join(['%s'] * len(data_list))
+                    query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+                    cursor.execute(query, data_list)
+                    connection.commit()
+            else:
+                table_name = id
+                create_query=f"CREATE TABLE {table_name} (question varchar(200) , difficulty varchar(15) ,choice integer, answer varchar(12) ,time decimal(20,17), hint varchar(200),code char(5))"
+                cursor.execute(create_query)
+                for i in user_responseList:
+                    data_list=i
+                    placeholders = ', '.join(['%s'] * len(data_list))
+                    query = f"INSERT INTO {table_name} VALUES ({placeholders})"
+                    cursor.execute(query, data_list)
+                    connection.commit()            
+            
         except Error as e:
             print(f'Error inserting user response: {e}')
         finally:
